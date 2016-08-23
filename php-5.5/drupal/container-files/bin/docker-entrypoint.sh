bd18be63c361fdd33408db7c057cc8cc8f7bef63
Lev Valuev <lev.valuev@gmail.com>
2016-08-23 17:33:41 +0300

    Добавил изменение юида пользователя апача

--- php-5.5/drupal/container-files/bin/docker-entrypoint.sh
+++ php-5.5/drupal/container-files/bin/docker-entrypoint.sh
@@ -4,15 +4,30 @@
 set -eo pipefail
 
 DE_TIMEZONE=Europe/Moscow
+DE_UID=1000
+DE_GID=1000
 
 if [[ ! -f /root/first_run ]]; then
     touch /root/first_run
-    if [[ -z $TIMEZONE ]]; then
+    if [[ -z ${TIMEZONE} ]]; then
         TIMEZONE=${DE_TIMEZONE}
     fi
+    
+    if [[ -z ${DUID} ]]; then
+        DUID=${DE_UID}
+    fi
+
+    if [[ -z ${DGID} ]]; then
+        DGID=${DE_GID}
+    fi
+
     #Let's set timezone
     echo "${TIMEZONE}" > /etc/timezone 
     dpkg-reconfigure -f noninteractive tzdata
+
+    #Let's change uid and git of apache user
+    usermod -u ${DUID} www-data
+    groupmod -g ${DGID} www-data
 fi
 
 exec "$@"
